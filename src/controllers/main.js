1bdeb4881aa0736f36425ff4c2705f3f767c15b6
micasalazar <micaela.salazar1586@gmail.com>
2023-12-08 22:38:08 -0300

    Desafio 1,2 y 3 finalizados

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,5 +1,6 @@
 const bcryptjs = require('bcryptjs');
 const db = require('../database/models');
+const {Op} = require('sequelize')
 
 const mainController = {
   home: (req, res) => {
@@ -11,16 +12,49 @@ const mainController = {
       })
       .catch((error) => console.log(error));
   },
-  bookDetail: (req, res) => {
-    // Implement look for details in the database
-    res.render('bookDetail');
+  bookDetail: async (req, res) => {
+     // Implement look for details in the database
+    try{
+      const bookFinded = await db.Book.findByPk(req.params.id, {
+        include: [{
+          model: db.Author,
+          as: 'authors' 
+        }]
+      })
+      if(bookFinded){
+        res.render('bookDetail', {book: bookFinded});        
+      }else{
+        res.status(404).send('Book not Found!')
+      }
+    }catch(error){
+      console.error("Error al buscar el libro" + error);
+      res.status(500).send('Error de servidor')
+    }   
   },
   bookSearch: (req, res) => {
     res.render('search', { books: [] });
   },
-  bookSearchResult: (req, res) => {
+  bookSearchResult: async (req, res) => {
     // Implement search by title
-    res.render('search');
+    try{
+      const {title} = req.body;
+      if(!title){
+        res.render('search', {books: []})
+      }
+      const books = await db.Book.findAll({
+        where:{
+          title:{
+            [db.Sequelize.Op.like]: `%${title.toUpperCase()}%`
+          }
+        },
+        include:[{association: 'authors'}]
+      });
+      // console.log('Libros encontrados:', books)
+      res.render('search', {books})
+    }catch(error){
+      console.error('Error al realizar la búsqueda ' + error);
+      res.status(500).send('Error de servidor')
+    }  
   },
   deleteBook: (req, res) => {
     // Implement delete book
@@ -33,10 +67,23 @@ const mainController = {
       })
       .catch((error) => console.log(error));
   },
-  authorBooks: (req, res) => {
+  authorBooks: async (req, res) => {
     // Implement books by author
-    res.render('authorBooks');
-  },
+    try{
+      const authorById = req.params.id;
+      const author = await db.Author.findByPk(authorById, {
+        include:[{ model: db.Book, as:'books', through:{attributes:[]}}]
+      });
+      if(author){
+        res.render('authorBooks', {author});
+      }else{
+        res.status(404).send('El autor no fuen encontrado')
+      }
+    }catch{
+      console.error('Error al obtener los libros del autor ' + error);
+      res.status(500).send('Error de servidor')
+    }
+    },
   register: (req, res) => {
     res.render('register');
   },
